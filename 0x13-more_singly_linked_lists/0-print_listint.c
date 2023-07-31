eea3ea49306546ba5d2105af25538541c5b8c376
Paree2512 <benjaminsemevor@ymail.com>
2023-07-31 14:12:16 -0700

    0-print_listint.c updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,14 +10,14 @@
 size_t print_listint(const listint_t *h)
 {
 
-size_t numb = 0;
+	size_t numb = 0;
 
-while (h)
-{
-	printf("%d\n", h->n);
-	numb++;
-	h = h->next;
-}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		numb++;
+		h = h->next;
+	}
 
-return (numb);
+	return (numb);
 }
